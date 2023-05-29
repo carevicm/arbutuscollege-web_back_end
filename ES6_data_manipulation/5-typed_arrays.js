05613f7a1114ccc36c8494e9606f06732b187864
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-05-28 18:19:01 -0700

    5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,9 @@
-function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0);
-
-  if (position > length - 1) {
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
     throw Error('Position outside range');
   }
-
-  view.setInt8(position, value);
-
-  return view;
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 }
-
-export default createInt8TypedArray;
